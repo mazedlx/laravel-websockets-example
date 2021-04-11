e91bf0de5f0846e03a79146c746e9f26c6cd9140
Christian Leo-Pernold <mazedlx@gmail.com>
2021-04-11 14:53:33 +0200

    WIP

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -26,7 +26,5 @@ window.Echo = new Echo({
     cluster: process.env.MIX_PUSHER_APP_CLUSTER,
     forceTLS: true,
     wsHost: window.location.hostname,
-    wsPort: 6001,
     wssPort: 6001,
-    enabledTransports: ['ws', 'wss'],
 });
